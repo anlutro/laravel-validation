a08537add03c9c164411452106985ea3cca89a1d
Andreas Lutro <anlutro@gmail.com>
2014-01-13 16:19:18 +0100

    added example

---
+++ examples/ProjectValidator.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * An example validator class for projects.
+ */
+class ProjectValidator extends \c\Validator
+{
+	public function __construct(ProjectModel $project)
+	{
+		parent::__construct($project);
+	}
+
+	/**
+	 * Get the common rules ran on every validation call.
+	 */
+	protected function getCommonRules()
+	{
+		return [
+			'title' => ['required', 'max:200'],
+			'start' => ['date_format:Y-m-d H:i:s'],
+			'deadline' => ['date_format:Y-m-d H:i:s'],
+		];
+	}
+
+	/**
+	 * These rules are only ran when creating a new project.
+	 */
+	protected function getCreateRules()
+	{
+		return [
+			'owner' => ['required', 'exists:users,id'],
+		];
+	}
+
+	/**
+	 * Prepare the rules before passing them to the validator.
+	 */
+	protected function prepareRules(array $rules, array $attributes)
+	{
+		// start and deadline are both optional, but if they're both set, we
+		// want to validate that the deadline is after the start.
+		if (!empty($attributes['start']) && !empty($attributes['deadline'])) {
+			// because we're validating that start and deadline are valid date/
+			// time strings, we can use the "after" validation rule.
+			$rules['deadline'][] = 'after:' . $attributes['start'];
+		}
+
+		// always return the modified rules!
+		return $rules;
+	}
+}
