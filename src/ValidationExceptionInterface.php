44c12d5a53071fa316e2968daa6f8616b5ae5d0a
Andreas Lutro <anlutro@gmail.com>
2014-08-26 10:37:48 +0200

    interface_exists is apparently a thing

--- src/ValidationExceptionInterface.php
+++ src/ValidationExceptionInterface.php
@@ -15,18 +15,18 @@ use Illuminate\Support\Contracts\JsonableInterface;
 use Illuminate\Support\Contracts\MessageProviderInterface;
 
 // 4.3 compatibility
-if (!class_exists('Illuminate\Support\Contracts\ArrayableInterface')
-	&& class_exists('Illuminate\Contracts\Support\ArrayableInterface')) {
+if (!interface_exists('Illuminate\Support\Contracts\ArrayableInterface')
+	&& interface_exists('Illuminate\Contracts\Support\ArrayableInterface')) {
 	class_alias('Illuminate\Contracts\Support\ArrayableInterface',
 		'Illuminate\Support\Contracts\ArrayableInterface');
 }
-if (!class_exists('Illuminate\Support\Contracts\JsonableInterface')
-	&& class_exists('Illuminate\Contracts\Support\JsonableInterface')) {
+if (!interface_exists('Illuminate\Support\Contracts\JsonableInterface')
+	&& interface_exists('Illuminate\Contracts\Support\JsonableInterface')) {
 	class_alias('Illuminate\Contracts\Support\JsonableInterface',
 		'Illuminate\Support\Contracts\JsonableInterface');
 }
-if (!class_exists('Illuminate\Support\Contracts\MessageProviderInterface')
-	&& class_exists('Illuminate\Contracts\Support\MessageProviderInterface')) {
+if (!interface_exists('Illuminate\Support\Contracts\MessageProviderInterface')
+	&& interface_exists('Illuminate\Contracts\Support\MessageProviderInterface')) {
 	class_alias('Illuminate\Contracts\Support\MessageProviderInterface',
 		'Illuminate\Support\Contracts\MessageProviderInterface');
 }
