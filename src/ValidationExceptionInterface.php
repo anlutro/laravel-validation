e1b9eb7fc05a02795971fe76fa7ec5c2cbf13ff4
Andreas Lutro <anlutro@gmail.com>
2014-08-26 10:29:03 +0200

    add checks to fix 4.3 tests

--- src/ValidationExceptionInterface.php
+++ src/ValidationExceptionInterface.php
@@ -14,6 +14,23 @@ use Illuminate\Support\Contracts\ArrayableInterface;
 use Illuminate\Support\Contracts\JsonableInterface;
 use Illuminate\Support\Contracts\MessageProviderInterface;
 
+// 4.3 compatibility
+if (!class_exists('Illuminate\Support\Contracts\ArrayableInterface')
+	&& class_exists('Illuminate\Contracts\Support\ArrayableInterface')) {
+	class_alias('Illuminate\Contracts\Support\ArrayableInterface',
+		'Illuminate\Support\Contracts\ArrayableInterface');
+}
+if (!class_exists('Illuminate\Support\Contracts\JsonableInterface')
+	&& class_exists('Illuminate\Contracts\Support\JsonableInterface')) {
+	class_alias('Illuminate\Contracts\Support\JsonableInterface',
+		'Illuminate\Support\Contracts\JsonableInterface');
+}
+if (!class_exists('Illuminate\Support\Contracts\MessageProviderInterface')
+	&& class_exists('Illuminate\Contracts\Support\MessageProviderInterface')) {
+	class_alias('Illuminate\Contracts\Support\MessageProviderInterface',
+		'Illuminate\Support\Contracts\MessageProviderInterface');
+}
+
 interface ValidationExceptionInterface extends
 	ArrayableInterface,
 	JsonableInterface,
