fb653cf29f230bf53beda4b35a4e1042d52faaeb
Andreas Lutro <anlutro@gmail.com>
2015-02-06 19:23:35 +0100

    5.0 compatibility

--- src/ValidationExceptionInterface.php
+++ src/ValidationExceptionInterface.php
@@ -14,6 +14,20 @@ use Illuminate\Support\Contracts\ArrayableInterface;
 use Illuminate\Support\Contracts\JsonableInterface;
 use Illuminate\Support\Contracts\MessageProviderInterface;
 
+// 5.x compatibility
+if (!interface_exists('Illuminate\Support\Contracts\ArrayableInterface')) {
+	class_alias('Illuminate\Contracts\Support\Arrayable',
+		'Illuminate\Support\Contracts\ArrayableInterface');
+}
+if (!interface_exists('Illuminate\Support\Contracts\JsonableInterface')) {
+	class_alias('Illuminate\Contracts\Support\Jsonable',
+		'Illuminate\Support\Contracts\JsonableInterface');
+}
+if (!interface_exists('Illuminate\Support\Contracts\MessageProviderInterface')) {
+	class_alias('Illuminate\Contracts\Support\MessageProvider',
+		'Illuminate\Support\Contracts\MessageProviderInterface');
+}
+
 interface ValidationExceptionInterface extends
 	ArrayableInterface,
 	JsonableInterface,
